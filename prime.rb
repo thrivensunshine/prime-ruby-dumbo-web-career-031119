df0498d1213a0a7efbba7e476310909da4fc7423
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-22 23:34:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(num)
+count = 1
+arr =[]
+arr2 =[]
+
+while num >= count
+arr << count
+count += 1
+end
+# puts arr
+
+arr.each do |i|
+if(num % i == 0)
+ arr2 << i
+puts "array2  #{arr2}"
+end
+end
+
+if arr2.length == 2
+return true
+
+else
+return false
+
+end
+end
+ 

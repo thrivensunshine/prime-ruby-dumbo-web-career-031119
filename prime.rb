450a1c080ddd17b072b57f09c6eb7d5d983c5636
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-22 23:01:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,19 @@
-# Add  code here!
-def prime?(num)
-  
+def test1(num)
+arr = []
+count = 1
+while num >= count 
+
+if(num % count == 0)
+arr << 1
+count += 1
+
+end
+end
+
+if(arr.length == 2)
+return true
+
+else
+return false
+end
+end
